1726d4e4fee2cdb4f9f89ed887ed99ef82c60264
Hardik Fumakiya <hardikfumakiya.97@gmail.com>
2020-05-10 19:15:21 +0530

    makecachematrix and cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL
+    
+    #set the value of the Matrix
+    setMatrix <- function(y) {
+      x <<- y
+      invMatrix <<- NULL
+    }
+    
+    getMatrix <- function() x                              #get the value of the Matrix
+    setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the invertible matrix
+    getInverse <- function() invMatrix                     #get the value of the invertible matrix
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse, getInverse = getInverse)
+    
 
 }
 
@@ -11,5 +25,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  #get the value of the invertible matrix from the makeCacheMatrix function
+  invMatrix <- x$getInverse()
+  if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL
+    message("Getting Cached Invertible Matrix")   #Type message: Getting Cached Invertible Matrix 
+    return(invMatrix)                             #return the invertible matrix
+  }
+  
+  #if value of the invertible matrix is NULL then  
+  MatrixData <- x$getMatrix()                     #get the original Matrix Data 
+  invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix
+  x$setInverse(invMatrix)                         #set the invertible matrix 
+  return(invMatrix)                               #return the invertible matrix
 }
+
+
